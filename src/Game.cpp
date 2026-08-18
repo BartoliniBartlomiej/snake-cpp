a2dad07341b7d8bdfa374e312715a353685e4736
bartek <bartek.k2906@gmail.com>
2026-08-18 21:58:46 +0200

    feat: add game turn movement handling

--- src/Game.cpp
+++ src/Game.cpp
@@ -4,14 +4,31 @@ Game::Game(Position initialSnakePosition, Direction initialDirection)
     : snake_{initialSnakePosition, initialDirection} {
 }
 
+void Game::updateGameState() {
+    if (!board_.isInside(snake_.head())) {
+        gameOver_ = true;
+    }
+}
+
 bool Game::isGameOver() const {
     return gameOver_;
 }
 
 void Game::moveForward() {
     snake_.moveForward();
+    updateGameState();
+}
 
-    if (!board_.isInside(snake_.head())) {
-        gameOver_ = true;
-    }
+void Game::moveLeft() {
+    snake_.moveLeft();
+    updateGameState();
+}
+
+void Game::moveRight() {
+    snake_.moveRight();
+    updateGameState();
+}
+
+const Snake& Game::snake() const {
+    return snake_;
 }
